09b03834a2711cc65bf3e709a63fefe948f25335
Mad3379 <apinanj82@gmail.com>
2024-09-20 10:31:49 +0700

    Create code.R

---
+++ programming/code.R
@@ -0,0 +1,7 @@
+INSERT INTO
+        Police (id ,name,department,benefit)
+  VALUES 
+        (1,'warm','SSD',100000),
+        (2,'Tid','CIBAOC',5000),
+        (3,'hi','TCSD',150000),
+        (4,'Pode','CIBAOC',50000)
